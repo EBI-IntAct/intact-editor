776642ef95b6e91df68c44e8fe0adfa4cc0489ee
oscar.forner.martinez@gmail.com <oscar.forner.martinez@gmail.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2015-01-07 13:44:15 +0000

    Finished the details and search. I have to improve the code of the exporter and make it like the editor

--- complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/ws/SearchController.java
+++ complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/ws/SearchController.java
@@ -11,25 +11,25 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import psidev.psi.mi.jami.datasource.InteractionWriter;
+import psidev.psi.mi.jami.factory.InteractionWriterFactory;
 import psidev.psi.mi.jami.factory.options.InteractionWriterOptions;
 import psidev.psi.mi.jami.json.InteractionViewerJson;
 import psidev.psi.mi.jami.json.nary.MIJsonModelledWriter;
-import psidev.psi.mi.jami.xml.PsiXmlVersion;
-import psidev.psi.mi.jami.xml.io.writer.compact.CompactXmlModelledWriter;
+import psidev.psi.mi.jami.model.ComplexType;
+import psidev.psi.mi.jami.model.InteractionCategory;
 import uk.ac.ebi.intact.dataexchange.psimi.solr.complex.ComplexFieldNames;
 import uk.ac.ebi.intact.jami.dao.IntactDao;
 import uk.ac.ebi.intact.jami.model.extension.IntactComplex;
 import uk.ac.ebi.intact.service.complex.ws.model.*;
 import uk.ac.ebi.intact.service.complex.ws.utils.IntactComplexUtils;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.stream.XMLStreamException;
 import java.io.StringWriter;
@@ -239,71 +239,72 @@ public class SearchController {
         return details;
     }
 
-    @RequestMapping(value = "/export/{ac}", method = RequestMethod.GET)
-    @Transactional(readOnly = true, propagation = Propagation.REQUIRED, value = "jamiTransactionManager")
-    public ResponseEntity<String> exportComplex(@PathVariable String ac, @RequestParam (required = false) String format) throws Exception {
-        IntactComplex complex = intactDao.getComplexDao().getByAc(ac);
-        ResponseEntity<String> responseEntity = null;
-        if (complex != null) {
-            if (format != null) {
-                switch (ComplexExportFormat.formatOf(format)) {
-                    case XML25:
-                        responseEntity = createXml25Response(complex);
-                        break;
-                    case XML30:
-                        responseEntity = createXml30Response(complex);
-                        break;
-                    case JSON:
-                    default:
-                        responseEntity = createJsonResponse(complex);
-                        break;
-                }
-            }
-            else {
-                responseEntity = createJsonResponse(complex);
-            }
-            return responseEntity;
-        }
-        throw new Exception("Complex " + ac + " not found");
-    }
-
-    private ResponseEntity<String> createXml25Response(IntactComplex complex) throws XMLStreamException {
-        return createXmlResponse(complex, PsiXmlVersion.v2_5_4);
-    }
-
-    private ResponseEntity<String> createXml30Response(IntactComplex complex) throws XMLStreamException {
-        return createXmlResponse(complex, PsiXmlVersion.v3_0_0);
-    }
-
-    private ResponseEntity<String> createXmlResponse(IntactComplex complex, PsiXmlVersion version) throws XMLStreamException {
-        StringWriter answer = new StringWriter();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        Map<String, Object> options = new HashMap<String, Object>();
-        CompactXmlModelledWriter writer = new CompactXmlModelledWriter(answer);
-        writer.initialiseContext(options);
-        writer.setVersion(version);
-        writer.start();
-        writer.write(complex);
-        writer.end();
-        writer.close();
-        httpHeaders.add("Content-Type:", "application/xml");
-        return new ResponseEntity<String>(answer.toString(), httpHeaders, HttpStatus.OK);
-    }
-
-    private ResponseEntity<String> createJsonResponse(IntactComplex complex) {
-        StringWriter answer = new StringWriter();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        Map<String, Object> options = new HashMap<String, Object>();
-        options.put(InteractionWriterOptions.OUTPUT_OPTION_KEY, answer);
-        MIJsonModelledWriter writer = new MIJsonModelledWriter();
-        writer.initialiseContext(options);
-        writer.start();
-        writer.write(complex);
-        writer.end();
-        writer.close();
-        httpHeaders.add("Content-Type:", "application/json");
-        return new ResponseEntity<String>(answer.toString(), httpHeaders, HttpStatus.OK);
-    }
+//    @RequestMapping(value = "/export/{ac}", method = RequestMethod.GET)
+//    @Transactional(readOnly = true, propagation = Propagation.REQUIRED, value = "jamiTransactionManager")
+//    public ResponseEntity<String> exportComplex(@PathVariable String ac, @RequestParam (required = false) String format) throws Exception {
+//        IntactComplex complex = intactDao.getComplexDao().getByAc(ac);
+//        ResponseEntity<String> responseEntity = null;
+//        if (complex != null) {
+//
+//            InteractionWriterFactory writerFactory = InteractionWriterFactory.getInstance();
+//            if (format != null) {
+//                switch (ComplexExportFormat.formatOf(format)) {
+//                    case XML25:
+//                        responseEntity = createXml25Response(complex, writerFactory);
+//                        break;
+//                    case XML30:
+//                        responseEntity = createXml30Response(complex, writerFactory);
+//                        break;
+//                    case JSON:
+//                    default:
+//                        responseEntity = createJsonResponse(complex, writerFactory);
+//                        break;
+//                }
+//            }
+//            else {
+//                responseEntity = createJsonResponse(complex, writerFactory);
+//            }
+//            return responseEntity;
+//        }
+//        throw new Exception("Complex " + ac + " not found");
+//    }
+//
+//    private ResponseEntity<String> createXml25Response(IntactComplex complex, InteractionWriterFactory writerFactory) throws XMLStreamException {
+//        return createXmlResponse(complex, writerFactory, PsiXmlVersion.v2_5_4);
+//    }
+//
+//    private ResponseEntity<String> createXml30Response(IntactComplex complex, InteractionWriterFactory writerFactory) throws XMLStreamException {
+//        return createXmlResponse(complex, writerFactory, PsiXmlVersion.v3_0_0);
+//    }
+//
+//    private ResponseEntity<String> createXmlResponse(IntactComplex complex, InteractionWriterFactory writerFactory, PsiXmlVersion version) throws XMLStreamException {
+//        IntactPsiXml.initialiseAllIntactXmlWriters();
+//        MIWriterOptionFactory optionFactory = MIWriterOptionFactory.getInstance();
+//        StringWriter answer = new StringWriter();
+//        InteractionWriter writer = writerFactory.getInteractionWriterWith(optionFactory.getDefaultCompactXmlOptions(complex, InteractionCategory.complex, ComplexType.n_ary, version)) ;
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        writer.start();
+//        writer.write(complex);
+//        writer.end();
+//        writer.close();
+//        httpHeaders.add("Content-Type:", "application/xml");
+//        return new ResponseEntity<String>(answer.toString(), httpHeaders, HttpStatus.OK);
+//    }
+//
+//    private ResponseEntity<String> createJsonResponse(IntactComplex complex, InteractionWriterFactory writerFactory) {
+//        StringWriter answer = new StringWriter();
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        Map<String, Object> options = new HashMap<String, Object>();
+//        options.put(InteractionWriterOptions.OUTPUT_OPTION_KEY, answer);
+//        MIJsonModelledWriter writer = new MIJsonModelledWriter();
+//        writer.initialiseContext(options);
+//        writer.start();
+//        writer.write(complex);
+//        writer.end();
+//        writer.close();
+//        httpHeaders.add("Content-Type:", "application/json");
+//        return new ResponseEntity<String>(answer.toString(), httpHeaders, HttpStatus.OK);
+//    }
 
     /*******************************/
     /*      Protected methods      */
--- complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/ws/utils/IntactComplexUtils.java
+++ complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/ws/utils/IntactComplexUtils.java
@@ -3,8 +3,9 @@ package uk.ac.ebi.intact.service.complex.ws.utils;
 import psidev.psi.mi.jami.model.*;
 import psidev.psi.mi.jami.utils.AliasUtils;
 import psidev.psi.mi.jami.utils.AnnotationUtils;
-import psidev.psi.mi.jami.utils.XrefUtils;
 import uk.ac.ebi.intact.jami.model.extension.IntactComplex;
+import uk.ac.ebi.intact.jami.model.extension.IntactModelledParticipant;
+import uk.ac.ebi.intact.jami.model.extension.InteractorXref;
 import uk.ac.ebi.intact.service.complex.ws.model.ComplexDetails;
 import uk.ac.ebi.intact.service.complex.ws.model.ComplexDetailsCrossReferences;
 import uk.ac.ebi.intact.service.complex.ws.model.ComplexDetailsFeatures;
@@ -35,9 +36,6 @@ public class IntactComplexUtils {
     public static final String CURATED_COMPLEX = "curated-complex";
     public static final String CURATED_COMPLEX_IA = "IA:0285";
 
-    public static final String BIO_ROLE = "biological role";
-    public static final String BIO_ROLE_MI = "MI:0500";
-
     public static final String INTACT = "intact";
     public static final String INTACT_MI = "MI:0469";
 
@@ -67,62 +65,82 @@ public class IntactComplexUtils {
     public static void setCrossReferences(IntactComplex complex, ComplexDetails details) {
         Collection<ComplexDetailsCrossReferences> crossReferences = details.getCrossReferences();
         ComplexDetailsCrossReferences cross;
-        //TODO check why wwpdb and Reactome cross references are not shown here
         for (Xref xref : complex.getXrefs()) {
-            cross = new ComplexDetailsCrossReferences();
-            if (xref.getDatabase() != null) {
-                cross.setDatabase(xref.getDatabase().getFullName());
-//            TODO check how I have to get the definition from an annotation
-//            cross.setDbdefinition(xref.getDatabase().getAnnotations());
-                cross.setDbMI(xref.getDatabase().getMIIdentifier());
-            }
-            if (xref.getQualifier() != null) {
-                cross.setQualifier(xref.getQualifier().getFullName());
-//            TODO check how I have to get the definition from an annotation
-//            cross.setQualifierDefinition(xref.getQualifier().getAnnotations());
-                cross.setQualifierMI(xref.getQualifier().getMIIdentifier());
-            }
-            cross.setIdentifier(xref.getId());
-            Annotation searchUrl = AnnotationUtils.collectFirstAnnotationWithTopic(xref.getDatabase().getAnnotations(), SEARCH_MI, SEARCH);
-            if (searchUrl != null) {
-                //TODO check why IntAct has to search url
-                cross.setSearchURL(searchUrl.getValue().replaceAll("\\$*\\{ac\\}",cross.getIdentifier()));
-            }
+            cross = createCrossReference(xref);
             crossReferences.add(cross);
         }
+        for (Xref xref : complex.getIdentifiers()) {
+            // We does not want to show us a cross references. That does not make sense.
+            if (! xref.getDatabase().getShortName().equals(INTACT)) {
+                cross = createCrossReference(xref);
+                crossReferences.add(cross);
+            }
+        }
+    }
+
+    private static ComplexDetailsCrossReferences createCrossReference(Xref xref) {
+        ComplexDetailsCrossReferences cross = new ComplexDetailsCrossReferences();
+        if (xref.getDatabase() != null) {
+            cross.setDatabase(xref.getDatabase().getFullName());
+            if (xref.getDatabase() instanceof OntologyTerm) {
+                OntologyTerm ontologyTerm = (OntologyTerm) xref.getDatabase();
+                if (ontologyTerm.getDefinition() != null)
+                    cross.setDbdefinition(ontologyTerm.getDefinition());
+            }
+            cross.setDbMI(xref.getDatabase().getMIIdentifier());
+        }
+        if (xref.getQualifier() != null) {
+            cross.setQualifier(xref.getQualifier().getFullName());
+            if (xref.getQualifier() instanceof OntologyTerm){
+                OntologyTerm ontologyTerm = (OntologyTerm) xref.getQualifier();
+                if (ontologyTerm.getDefinition() != null)
+                    cross.setQualifierDefinition(ontologyTerm.getDefinition());
+            }
+            cross.setQualifierMI(xref.getQualifier().getMIIdentifier());
+        }
+        cross.setIdentifier(xref.getId());
+        Annotation searchUrl = AnnotationUtils.collectFirstAnnotationWithTopic(xref.getDatabase().getAnnotations(), SEARCH_MI, SEARCH);
+        if (searchUrl != null) {
+            cross.setSearchURL(searchUrl.getValue().replaceAll("\\$*\\{ac\\}",cross.getIdentifier()));
+        }
+        if (xref instanceof InteractorXref) {
+            InteractorXref interactorXref = (InteractorXref) xref;
+            if (interactorXref.getSecondaryId() != null) cross.setDescription(interactorXref.getSecondaryId());
+        }
+        return cross;
     }
 
     // This method fills the participants table for the view
     public static void setParticipants(IntactComplex complex, ComplexDetails details) {
         Collection<ComplexDetailsParticipants> participants = details.getParticipants();
         ComplexDetailsParticipants part;
-        for (Participant participant : complex.getParticipants()) {
+        for (ModelledParticipant participant : complex.getParticipants()) { //Use ModelledParticipant
             part = new ComplexDetailsParticipants();
             Interactor interactor = participant.getInteractor();
+
             if (interactor != null) {
-                part.setInteractorAC(XrefUtils.collectFirstIdentifierWithDatabase(interactor.getIdentifiers(), INTACT_MI, INTACT).getId());
                 setInteractorType(part, interactor);
                 part.setDescription(interactor.getFullName());
+                part.setInteractorAC(((IntactModelledParticipant) participant).getAc());
                 if (interactor instanceof Protein) {
-                    Alias alias = AliasUtils.collectFirstAliasWithType(interactor.getAliases(), Alias.COMPLEX_SYNONYM_MI, Alias.COMPLEX_SYNONYM);
-                    part.setName(alias != null ? alias.getName() : ((Protein) interactor).getGeneName());
-                    part.setIdentifier(interactor.getPreferredIdentifier().getId());
+                    Protein protein = (Protein) interactor;
+                    Alias alias = AliasUtils.collectFirstAliasWithType(protein.getAliases(), Alias.COMPLEX_SYNONYM_MI, Alias.COMPLEX_SYNONYM);
+                    part.setName(alias != null ? alias.getName() : protein.getGeneName());
+                    part.setIdentifier(protein.getPreferredIdentifier().getId());
                 }
                 else if (interactor instanceof BioactiveEntity) {
-                    part.setName(interactor.getShortName());
-                    part.setIdentifier(((BioactiveEntity) interactor).getChebi());
+                    BioactiveEntity bioactiveEntity = (BioactiveEntity) interactor;
+                    part.setName(bioactiveEntity.getShortName());
+                    part.setIdentifier(bioactiveEntity.getChebi());
                 }
                 else {
                     part.setName(interactor.getShortName());
                     part.setIdentifier(interactor.getFullName());
                 }
-                //TODO Identifier Link
-//                part.setIdentifier(xref.getPrimaryId());
-//                for ( Annotation annotation : xref.getCvDatabase().getAnnotations() ) {
-//                    if ( annotation.getCvTopic() != null && CvTopic.SEARCH_URL_MI_REF.equals(annotation.getCvTopic().getIdentifier()) ) {
-//                        part.setIdentifierLink(annotation.getAnnotationText().replaceAll("\\$*\\{ac\\}", xref.getPrimaryId()));
-//                    }
-//                }
+                Annotation searchUrl = AnnotationUtils.collectFirstAnnotationWithTopic(interactor.getPreferredIdentifier().getDatabase().getAnnotations(), SEARCH_MI, SEARCH);
+                if (searchUrl != null) {
+                    part.setIdentifierLink(searchUrl.getValue().replaceAll("\\$*\\{ac\\}", part.getIdentifier()));
+                }
                 part.setStochiometry(participant.getStoichiometry().toString());
                 if (participant.getBiologicalRole() != null) {
                     setBiologicalRole(part, participant);
@@ -140,8 +158,11 @@ public class IntactComplexUtils {
                 ComplexDetailsFeatures complexDetailsFeatures = new ComplexDetailsFeatures();
                 part.getLinkedFeatures().add(complexDetailsFeatures);
                 complexDetailsFeatures.setFeatureType(linked.getType().getShortName());
-                //TODO check how I have to get the definition from an annotation
-                //complexDetailsFeatures.setFeatureTypeDefinition(linked.getType().getAnnotations().toString());
+                if (linked.getType() instanceof OntologyTerm) {
+                    OntologyTerm ontologyTerm = (OntologyTerm) linked.getType();
+                    if (ontologyTerm.getDefinition() != null)
+                        complexDetailsFeatures.setFeatureTypeDefinition(ontologyTerm.getDefinition());
+                }
                 complexDetailsFeatures.setFeatureTypeMI(linked.getType().getMIIdentifier());
                 complexDetailsFeatures.setParticipantId(linked.getParticipant().getInteractor().getPreferredIdentifier().getId());
                 for (Range range : (List<Range>) linked.getRanges()) {
@@ -158,10 +179,10 @@ public class IntactComplexUtils {
         CvTerm term = interactor.getInteractorType();
         part.setInteractorType(term.getFullName());
         part.setInteractorTypeMI(term.getMIIdentifier());
-        //TODO check how I have to get the definition from an annotation
-        Annotation annotation = AnnotationUtils.collectFirstAnnotationWithTopic(term.getAnnotations(), Annotation.COMPLEX_PROPERTIES, Annotation.COMPLEX_PROPERTIES_MI);
-        if (annotation != null) {
-            part.setInteractorTypeDefinition(annotation.getValue());
+        if (term instanceof OntologyTerm) {
+            OntologyTerm ontologyTerm = (OntologyTerm) term;
+            if (ontologyTerm.getDefinition() != null)
+                part.setInteractorTypeDefinition(ontologyTerm.getDefinition());
         }
     }
 
@@ -170,10 +191,10 @@ public class IntactComplexUtils {
         CvTerm term = participant.getBiologicalRole();
         part.setBioRole(term.getFullName());
         part.setBioRoleMI(term.getMIIdentifier());
-        //TODO check how I have to get the definition from an annotation
-        Annotation annotation = AnnotationUtils.collectFirstAnnotationWithTopic(term.getAnnotations(), BIO_ROLE_MI, BIO_ROLE);
-        if (annotation != null) {
-            part.setBioRoleDefinition(annotation.getValue());
+        if (term instanceof OntologyTerm) {
+            OntologyTerm ontologyTerm = (OntologyTerm) term;
+            if (ontologyTerm.getDefinition() != null)
+                part.setBioRoleDefinition(ontologyTerm.getDefinition());
         }
     }
 
