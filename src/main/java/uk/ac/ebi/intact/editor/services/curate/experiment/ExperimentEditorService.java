0633312e1392b4b5142482f542d3dfd0edfab566
maximiliankoch <tschaka1904@gmail.com>
2015-08-27 13:22:26 +0100

    Added methods of ExperimentDetailedViewService into ExperimentEditorService, since we show all the information in just one view.

--- src/main/java/uk/ac/ebi/intact/editor/services/curate/experiment/ExperimentEditorService.java
+++ src/main/java/uk/ac/ebi/intact/editor/services/curate/experiment/ExperimentEditorService.java
@@ -24,6 +24,8 @@ import psidev.psi.mi.jami.utils.AnnotationUtils;
 import psidev.psi.mi.jami.utils.CvTermUtils;
 import uk.ac.ebi.intact.editor.controller.curate.cloner.ExperimentCloner;
 import uk.ac.ebi.intact.editor.controller.curate.cloner.InteractionEvidenceCloner;
+import uk.ac.ebi.intact.editor.controller.curate.cloner.InteractorCloner;
+import uk.ac.ebi.intact.editor.controller.curate.experiment.ExperimentWrapper;
 import uk.ac.ebi.intact.editor.services.AbstractEditorService;
 import uk.ac.ebi.intact.jami.model.extension.*;
 import uk.ac.ebi.intact.jami.model.lifecycle.Releasable;
@@ -218,6 +220,89 @@ public class ExperimentEditorService extends AbstractEditorService {
         }
 
         return pub;
+    }
+
+    @Transactional(value = "jamiTransactionManager", propagation = Propagation.REQUIRED, readOnly = true)
+    public ExperimentWrapper loadExperimentWrapperByAc( String ac ) {
+        IntactExperiment experiment = this.loadExperimentByAc(ac);
+
+        if (experiment != null) {
+            return new ExperimentWrapper(experiment);
+        } else {
+            return null;
+        }
+    }
+
+    @Transactional(value = "jamiTransactionManager", propagation = Propagation.REQUIRED, readOnly = true)
+    public ExperimentWrapper loadExperimentWrapper( IntactExperiment experiment ) {
+        if (!isExperimentFullyInitialised(experiment)
+                && experiment.getAc() != null
+                && !getIntactDao().getEntityManager().contains(experiment)){
+            return loadExperimentWrapperByAc(experiment.getAc());
+        }
+
+        InteractorCloner cloner = new InteractorCloner();
+        for (InteractionEvidence inter : experiment.getInteractionEvidences()){
+            for (ParticipantEvidence part : inter.getParticipants()){
+                initialiseParticipant(part, cloner);
+            }
+        }
+
+        return new ExperimentWrapper(experiment);
+    }
+
+    private void initialiseParticipant(ParticipantEvidence det, InteractorCloner cloner) {
+        IntactInteractor interactor = (IntactInteractor)det.getInteractor();
+        if (!isInteractorInitialised(interactor)){
+            IntactInteractor interactorReloaded = (IntactInteractor)initialiseInteractor(interactor, cloner);
+            if (interactorReloaded != interactor){
+                det.setInteractor(interactor);
+            }
+        }
+        initialiseXrefs(interactor.getDbXrefs());
+        initialiseAnnotations(interactor.getDbAnnotations());
+    }
+
+    private boolean isExperimentFullyInitialised(IntactExperiment experiment){
+
+        if (!experiment.areInteractionEvidencesInitialized() || !experiment.areAnnotationsInitialized()){
+            return false;
+        }
+        else{
+            for (InteractionEvidence inter : experiment.getInteractionEvidences()){
+                if (inter instanceof IntactInteractionEvidence){
+                    IntactInteractionEvidence ev = (IntactInteractionEvidence)inter;
+
+                    if (!ev.areParticipantsInitialized()){
+                        return false;
+                    }
+                    else if (!ev.areXrefsInitialized() || !ev.areParametersInitialized() || !ev.areAnnotationsInitialized()){
+                        return false;
+                    }
+                    else{
+                        for (ParticipantEvidence p : ev.getParticipants()){
+                            if (p instanceof IntactParticipantEvidence){
+                                IntactParticipantEvidence part = (IntactParticipantEvidence)p;
+                                if (!part.areFeaturesInitialized()){
+                                    return false;
+                                }
+                                else{
+                                    for (FeatureEvidence f : part.getFeatures()){
+                                        if (f instanceof IntactFeatureEvidence){
+                                            IntactFeatureEvidence feat = (IntactFeatureEvidence)f;
+                                            if (!feat.areLinkedFeaturesInitialized() || !feat.areRangesInitialized()){
+                                                return false;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     private void initialiseEvidences(IntactExperiment parent, Collection<InteractionEvidence> evidences) {
