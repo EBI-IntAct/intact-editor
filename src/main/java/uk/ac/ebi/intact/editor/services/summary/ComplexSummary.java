0741460b488a4f59feab86773b6325a3643dda0c
jmedinaebi <jmedina@ebi.ac.uk>
2024-09-24 13:02:16 +0100

    Display complex AC as participant and on complexes table

--- src/main/java/uk/ac/ebi/intact/editor/services/summary/ComplexSummary.java
+++ src/main/java/uk/ac/ebi/intact/editor/services/summary/ComplexSummary.java
@@ -13,6 +13,7 @@ import java.io.Serializable;
 public class ComplexSummary implements Serializable{
     private String rowStyle;
     private String ac;
+    private String complexAc;
     private String name;
     private String caution;
     private String internalRemark;
@@ -38,6 +39,14 @@ public class ComplexSummary implements Serializable{
 
     public void setAc(String ac) {
         this.ac = ac;
+    }
+
+    public String getComplexAc() {
+        return complexAc;
+    }
+
+    public void setComplexAc(String complexAc) {
+        this.complexAc = complexAc;
     }
 
     public String getName() {
